739622176c4cc1da6abd4e6117dce0a897f55ddd
Halle Williams and Matthew Giem <halle+matthew@>
2016-06-15 09:59:02 -0700

    create the random number generator for our dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 //  Front End logic
+$(document).ready(function()  {
+  $("#displayDice").text(getRandomIntInclusive(1,6));
+})
 
 // Back End logic
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
