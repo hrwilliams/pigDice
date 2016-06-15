0962ace72eca3703675fbdb4eee4abf2d1f63ff8
Halle Williams and Matthew Giem <halle+matthew@>
2016-06-15 16:36:29 -0700

    style the site while adding images for the roll butten and give the die value as a pic of the die instead of just a number

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 //  Front End logic
 var turnScore;
+var diceArray = [];
 $(document).ready(function()  {
   $("#player2").hide();
+  $("#displayDice").hide();
+  $("#displayDice2").hide();
   var player1 = new PlayerScore("player1",0);
   var player2 = new PlayerScore("player2",0);
   var roll;
   var turnScore = 0;
   $("#rollDice").click(function() {
     roll = getRandomIntInclusive(1,6);
-    $("#displayDice").text(roll);
+    toggleDie(roll);
     if (roll === 1) {
       $("#turnScore").text(turnScore);
       $("#totalScore").text(player1.totalScore);
       alert("end of turn");
-      $("#displayDice2").text("");
+      // $("#displayDice2").text("");
       $("#turnScore2").text("");
       $("#player1").hide();
       $("#player2").show();
@@ -40,7 +43,7 @@ $(document).ready(function()  {
       $("#totalScore2").text("");
 
     }
-    $("#displayDice2").text("");
+    // $("#displayDice2").text("");
     $("#turnScore2").text("");
     $("#player1").hide();
     $("#player2").show();
@@ -53,12 +56,12 @@ $(document).ready(function()  {
   var turnScore2 = 0;
   $("#rollDice2").click(function() {
     roll = getRandomIntInclusive(1,6);
-    $("#displayDice2").text(roll);
+    toggleDie2(roll);
     if (roll === 1) {
       $("#turnScore2").text(turnScore2);
       $("#totalScore2").text(player2.totalScore);
-      alert("end of turn");
-      $("#displayDice").text("");
+      alert("End of turn");
+      // $("#displayDice").text("");
       $("#turnScore").text("");
       $("#player1").show();
       $("#player2").hide();
@@ -83,7 +86,7 @@ $(document).ready(function()  {
       $("#totalScore").text("");
       $("#totalScore2").text("");
     }
-    $("#displayDice").text("");
+    // $("#displayDice").text("");
     $("#turnScore").text("");
     $("#player1").show();
     $("#player2").hide();
@@ -107,4 +110,129 @@ function PlayerScore (player, totalScore) {
 
 PlayerScore.prototype.endOfTurn = function(score) {
   return this.totalScore = this.totalScore + score;
+}
+
+var toggleDie = function(roll)  {
+  if (roll === 1) {
+    $("#displayDice").show();
+    $("#side6").hide();
+    $("#side2").hide();
+    $("#side3").hide();
+    $("#side4").hide();
+    $("#side5").hide();
+    $("#displayDice2").hide();
+    $("#side1").show();
+  }
+  else if (roll === 2) {
+    $("#displayDice").show();
+    $("#side1").hide();
+    $("#side6").hide();
+    $("#side3").hide();
+    $("#side4").hide();
+    $("#side5").hide();
+    $("#displayDice2").hide();
+    $("#side2").show();
+  }
+  else if (roll === 3) {
+    $("#displayDice").show();
+    $("#side1").hide();
+    $("#side2").hide();
+    $("#side6").hide();
+    $("#side4").hide();
+    $("#side5").hide();
+    $("#displayDice2").hide();
+    $("#side3").show();
+  }
+  else if (roll === 4) {
+    $("#displayDice").show();
+    $("#side1").hide();
+    $("#side2").hide();
+    $("#side3").hide();
+    $("#side6").hide();
+    $("#side5").hide();
+    $("#displayDice2").hide();
+    $("#side4").show();
+  }
+  else if (roll === 5) {
+    $("#displayDice").show();
+    $("#side1").hide();
+    $("#side2").hide();
+    $("#side3").hide();
+    $("#side4").hide();
+    $("#side6").hide();
+    $("#displayDice2").hide();
+    $("#side5").show();
+  }
+  else if (roll === 6) {
+    $("#displayDice").show();
+    $("#side1").hide();
+    $("#side2").hide();
+    $("#side3").hide();
+    $("#side4").hide();
+    $("#side5").hide();
+    $("#displayDice2").hide();
+    $("#side6").show();
+  }
+}
+var toggleDie2 = function(roll)  {
+  if (roll === 1) {
+    $("#displayDice2").show();
+    $("#2side6").hide();
+    $("#2side2").hide();
+    $("#2side3").hide();
+    $("#2side4").hide();
+    $("#2side5").hide();
+    $("#displayDice").hide();
+    $("#2side1").show();
+  }
+  else if (roll === 2) {
+    $("#displayDice2").show();
+    $("#2side1").hide();
+    $("#2side6").hide();
+    $("#2side3").hide();
+    $("#2side4").hide();
+    $("#2side5").hide();
+    $("#displayDice").hide();
+    $("#2side2").show();
+  }
+  else if (roll === 3) {
+    $("#displayDice2").show();
+    $("#2side1").hide();
+    $("#2side2").hide();
+    $("#2side6").hide();
+    $("#2side4").hide();
+    $("#2side5").hide();
+    $("#displayDice").hide();
+    $("#2side3").show();
+  }
+  else if (roll === 4) {
+    $("#displayDice2").show();
+    $("#2side1").hide();
+    $("#2side2").hide();
+    $("#2side3").hide();
+    $("#2side6").hide();
+    $("#2side5").hide();
+    $("#displayDice").hide();
+    $("#2side4").show();
+  }
+  else if (roll === 5) {
+    $("#displayDice2").show();
+    $("#2side1").hide();
+    $("#2side2").hide();
+    $("#2side3").hide();
+    $("#2side4").hide();
+    $("#2side6").hide();
+    $("#displayDice").hide();
+    $("#2side5").show();
+  }
+  else if (roll === 6) {
+    $("#displayDice2").show();
+    $("#2side1").hide();
+    $("#2side2").hide();
+    $("#2side3").hide();
+    $("#2side4").hide();
+    $("#2side5").hide();
+    $("#displayDice").hide();
+    $("#2side6").show();
+  }
 }
