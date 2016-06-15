e8b212dd2b15c80eccb784dee1c017be9d52b201
Halle Williams and Matthew Giem <halle+matthew@>
2016-06-15 13:50:59 -0700

    create a second player for the pig dice game and fix a couple bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 //  Front End logic
 var turnScore;
 $(document).ready(function()  {
-  var player1 = new PlayerScore("player",0);
+  $("#player2").hide();
+  var player1 = new PlayerScore("player1",0);
+  var player2 = new PlayerScore("player2",0);
   var roll;
   var turnScore = 0;
   $("#rollDice").click(function() {
     roll = getRandomIntInclusive(1,6);
     $("#displayDice").text(roll);
     if (roll === 1) {
-      turnScore = 0;
       $("#turnScore").text(turnScore);
+      $("#totalScore").text(player1.totalScore);
       alert("end of turn");
+      $("#displayDice2").text("");
+      $("#turnScore2").text("");
+      $("#player1").hide();
+      $("#player2").show();
+      if (player2.totalScore != 0) {
+          $("#totalScore2").text(player2.totalScore);
+      }
+      turnScore = 0;
     }
     else {
       turnScore += roll;
@@ -19,9 +29,51 @@ $(document).ready(function()  {
   });
   $("#hold").click(function(){
     player1.endOfTurn(turnScore);
-    turnScore = 0;
     $("#turnScore").text(turnScore);
-    alert(player1.totalScore);
+    $("#totalScore").text(player1.totalScore);
+    $("#displayDice2").text("");
+    $("#turnScore2").text("");
+    $("#player1").hide();
+    $("#player2").show();
+    if (player2.totalScore != 0) {
+        $("#totalScore2").text(player2.totalScore);
+    }
+    turnScore = 0;
+  })
+  var turnScore2 = 0;
+  $("#rollDice2").click(function() {
+    roll = getRandomIntInclusive(1,6);
+    $("#displayDice2").text(roll);
+    if (roll === 1) {
+      $("#turnScore2").text(turnScore2);
+      $("#totalScore2").text(player2.totalScore);
+      alert("end of turn");
+      $("#displayDice").text("");
+      $("#turnScore").text("");
+      $("#player1").show();
+      $("#player2").hide();
+      if (player1.totalScore != 0) {
+          $("#totalScore").text(player1.totalScore);
+      }
+      turnScore2 = 0;
+    }
+    else {
+      turnScore2 += roll;
+      $("#turnScore2").text(turnScore2);
+    }
+  });
+  $("#hold2").click(function(){
+    player2.endOfTurn(turnScore2);
+    $("#turnScore2").text(turnScore2);
+    $("#totalScore2").text(player2.totalScore);
+    $("#displayDice").text("");
+    $("#turnScore").text("");
+    $("#player1").show();
+    $("#player2").hide();
+    if (player1.totalScore != 0) {
+        $("#totalScore").text(player1.totalScore);
+      }
+    turnScore2 = 0;
   })
 })
 
